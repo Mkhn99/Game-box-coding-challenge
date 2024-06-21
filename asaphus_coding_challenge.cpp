d7616273fbdb89d1a18f347df4dd6a29c8ae8608
Mkhn99 <moin111282@gmail.com>
2024-06-21 22:35:22 +0200

    Added player's turn taking logic

--- asaphus_coding_challenge.cpp
+++ asaphus_coding_challenge.cpp
@@ -5,16 +5,16 @@
  *
  * The goal is to implement the mechanics of a simple game and write test cases for them.
  * The rules of the game are:
- * - There are two types of boxes, green and blue. 
- * - Both can absorb tokens of a given weight, which they add to their own total weight. 
+ * - There are two types of boxes, green and blue.
+ * - Both can absorb tokens of a given weight, which they add to their own total weight.
  * - Both are initialized with a given initial weight.
- * - After a box absorbs a token weight, it outputs a score. 
+ * - After a box absorbs a token weight, it outputs a score.
  * - Green and blue boxes calculate the score in different ways:
  * - A green box calculates the score as the square of the mean of the 3 weights that it most recently absorbed (square of mean of all absorbed weights if there are fewer than 3).
  * - A blue box calculates the score as Cantor's pairing function of the smallest and largest weight that it has absorbed so far, i.e. pairing(smallest, largest), where pairing(0, 1) = 2
  * - The game is played with two green boxes with initial weights 0.0 and 0.1, and two blue boxes with initial weights 0.2 and 0.3.
  * - There is a list of input token weights. Each gets used in one turn.
- * - There are two players, A and B. Both start with a score of 0. 
+ * - There are two players, A and B. Both start with a score of 0.
  * - The players take turns alternatingly. Player A starts.
  * - In each turn, the current player selects one of the boxes with the currently smallest weight, and lets it absorb the next input token weight. Each input weight gets only used once.
  * - The result of the absorption gets added to the current player's score.
@@ -47,6 +47,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+enum class BoxColor { BLUE, GREEN };
+
 class Box {
  public:
   explicit Box(double initial_weight) : weight_(initial_weight) {}
@@ -54,13 +56,27 @@ class Box {
   static std::unique_ptr<Box> makeBlueBox(double initial_weight);
   bool operator<(const Box& rhs) const { return weight_ < rhs.weight_; }
 
-  // TODO
+  BoxColor getBoxColor() const { return color_; }
+	void setBoxColor(BoxColor newColor) { color_ = newColor; }
 
  protected:
   double weight_;
+  BoxColor color_ = BoxColor::BLUE;
 };
 
-// TODO
+//Initializing a green box
+std::unique_ptr<Box> Box::makeGreenBox(double initial_weight) {
+	std::unique_ptr<Box> greenBox = std::make_unique<Box>(initial_weight);
+	greenBox->setBoxColor(BoxColor::GREEN);
+	return greenBox;
+}
+
+//Initializing a blue box
+std::unique_ptr<Box> Box::makeBlueBox(double initial_weight) {
+	std::unique_ptr<Box> blueBox = std::make_unique<Box>(initial_weight);
+	blueBox->setBoxColor(BoxColor::BLUE);
+	return blueBox;
+}
 
 class Player {
  public:
@@ -75,17 +91,28 @@ class Player {
 };
 
 std::pair<double, double> play(const std::vector<uint32_t>& input_weights) {
-  std::vector<std::unique_ptr<Box> > boxes;
-  boxes.emplace_back(Box::makeGreenBox(0.0));
-  boxes.emplace_back(Box::makeGreenBox(0.1));
-  boxes.emplace_back(Box::makeBlueBox(0.2));
-  boxes.emplace_back(Box::makeBlueBox(0.3));
+	std::vector<std::unique_ptr<Box>> boxes;
+	boxes.emplace_back(Box::makeGreenBox(0.0));
+	boxes.emplace_back(Box::makeGreenBox(0.1));
+	boxes.emplace_back(Box::makeBlueBox(0.2));
+	boxes.emplace_back(Box::makeBlueBox(0.3));
 
-  // TODO
+	Player player_A, player_B;
 
-  std::cout << "Scores: player A " << player_A.getScore() << ", player B "
-            << player_B.getScore() << std::endl;
-  return std::make_pair(player_A.getScore(), player_B.getScore());
+	//Players taking turn mechanism
+	bool player_A_turn = true;
+	for (uint32_t weight : input_weights) {
+		if (player_A_turn) {
+			player_A.takeTurn(weight, boxes);
+		}
+		else {
+			player_B.takeTurn(weight, boxes);
+		}
+		player_A_turn = !player_A_turn;
+	}
+
+	std::cout << "Scores: player A " << player_A.getScore() << ", player B " << player_B.getScore() << std::endl;
+	return std::make_pair(player_A.getScore(), player_B.getScore());
 }
 
 TEST_CASE("Final scores for first 4 Fibonacci numbers", "[fibonacci4]") {
